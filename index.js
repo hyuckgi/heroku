e71c3c8df92ed3762142afc08611e269c1d50ae7
hyuckgi <gadokkwon@gmail.com>
2017-03-13 15:39:47 +0900

    test2

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ var express = require('express');
 var pg = require('pg');
 var app = express();
 
-
-
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL,function(err, client) {
-    if(err) throw err;
-    console.log("Connected to postgres! Getting schemas");
 
-    client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-        console.log(JSON.stringify(row));
-    });
-})
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
