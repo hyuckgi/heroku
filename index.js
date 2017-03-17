ac1351a01724f9855d385305f307b10068d1648f
hyuckgi <gadokkwon@gmail.com>
2017-03-17 10:34:52 +0900

    Demo

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
-var pg = require('pg');
 var app = express();
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
 
 
 // views is directory for all template files
@@ -26,6 +14,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+    response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
