ad8c5bb301b70877998359fcb0a7ef276ce673c7
hyuckgi <gadokkwon@gmail.com>
2017-03-17 12:29:06 +0900

    db test

--- index.js
+++ index.js
@@ -14,10 +14,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-mongoose.connect(process.env.MONGODB_URI);
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
-// define model
-var Books = require('./models/books');
+// mongoose.connect(process.env.MONGODB_URI);
+var mongodbUri = 'mongodb://gadokkwon:sksmsskdi9232@ds133670.mlab.com:33670/heroku_72r92033';
+
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;
+
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+conn.once('open', function() {
+  console.log("hello mongodb");
+});
+
+// // define model
+// var Books = require('./models/books');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -27,12 +40,12 @@ app.get('/cool', function(request, response) {
     response.send(cool());
 });
 
-app.get('/books', function(req, res) {
-    Books.find(function(err, books) {
-        if(err) return res.status(500).send({error: 'database failure'});
-        res.json(books);
-    });
-});
+// app.get('/books', function(req, res) {
+//     Books.find(function(err, books) {
+//         if(err) return res.status(500).send({error: 'database failure'});
+//         res.json(books);
+//     });
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
